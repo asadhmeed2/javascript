5da49bc8c529b97122b668ee0a1a2e6b382d9b85
asadhmeed <asadhmeed1@gmail.com>
2021-09-12 12:49:25 +0300

    ex-2.1

--- js-ex/ex2.1/ex2.1-function.js
+++ js-ex/ex2.1/ex2.1-function.js
@@ -3,7 +3,9 @@ return `${country} has ${population} people and its capital city is ${capitalCit
 }
 
 
-
-console.log(describeCountry("Isreal" , "874,186","Jerusalem" ));
-console.log(describeCountry("Usa" , "692,683","Washington" ));
-console.log(describeCountry("England" , "8.982 milion","London" ));
+let is=describeCountry("Isreal" , "874,186","Jerusalem" );
+let usa=describeCountry("Usa" , "692,683","Washington" );
+let eng=describeCountry("England" , "8.982 milion","London" );
+console.log(is);
+console.log(usa);
+console.log(eng);
