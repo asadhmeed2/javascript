90bbd6a896d62ace2d608ae474e5cd96c4726295
asadhmeed <asadhmeed1@gmail.com>
2021-09-12 12:46:40 +0300

    ex-2.1

---
+++ js-ex/ex2.1/ex2.1-function.js
@@ -0,0 +1,9 @@
+function describeCountry(country, population ,capitalCity){
+return `${country} has ${population} people and its capital city is ${capitalCity}`;
+}
+
+
+
+console.log(describeCountry("Isreal" , "874,186","Jerusalem" ));
+console.log(describeCountry("Usa" , "692,683","Washington" ));
+console.log(describeCountry("England" , "8.982 milion","London" ));
