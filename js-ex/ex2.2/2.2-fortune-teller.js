38dfec09910db6fe1982cd6f418e9cba5f3ba098
asadhmeed <asadhmeed1@gmail.com>
2021-09-12 12:55:45 +0300

    ex-2.2

---
+++ js-ex/ex2.2/2.2-fortune-teller.js
@@ -0,0 +1,3 @@
+function tellFortune(jobIitle,location,partnerName,numberOfChildren){
+    return `You will bi a ${jobIitle} in ${location} and married to ${partnerName} with ${numberOfChildren} children.`;
+}
