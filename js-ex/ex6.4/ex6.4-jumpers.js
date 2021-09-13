3e47c0844175d85147d7f7401d5bf374a1e878f1
asadhmeed <asadhmeed1@gmail.com>
2021-09-13 22:52:37 +0300

    update ex6.4

--- js-ex/ex6.4/ex6.4-jumpers.js
+++ js-ex/ex6.4/ex6.4-jumpers.js
@@ -1,30 +1,41 @@
-const jumps = [5, 5, 8, 7, -1, 5,10,8,9];
+const jumps = [5, 5, 8, 7, -1, 5, 10, 8, 9];
+const jumps2 = [-1, -1,-1, 7, -1, 5, 10, 8, 9];
 
 // every jump is 3 array element
 
 function avgJumper(arr) {
     let avgJump = [];
-    
+
     for (let i = 0; i < arr.length; i += 3) {
-        avgJump.push((arr[i] + ((arr[i + 1] !== -1) ? arr[i + 1] : 0) + arr[i + 2]) / ((arr[i + 1] !== -1) ? 3 : 2));
+        let count = 0;
+        let newArr = arr.slice(i, i + 3);
+        newArr.forEach(e => {
+            (e !== -1) ? count++ : 0;
+        });
+
+        avgJump.push((((arr[i] !== -1) ? arr[i] : 0) +
+            ((arr[i + 1] !== -1) ? arr[i + 1] : 0) 
+            + ((arr[i + 2] !== -1) ? arr[i + 2] : 0)) /
+            ((count === 0) ? 1 : count))
     }
     return avgJump;
 }
 
 console.log(avgJumper(jumps));
 
-function bestAvgJump(avgjump){
 
-    let best=0;
+function bestAvgJump(avgjump) {
+
+    let best = 0;
     avgjump.forEach(e => {
-        if(e > best){
-            best=e;
+        if (e > best) {
+            best = e;
         }
     });
     return best;
 }
 
-console.log("best avg jumper is :",bestAvgJump(avgJumper(jumps)));
+console.log("best avg jumper is :", bestAvgJump(avgJumper(jumps)));
 
 
 function bestJump(jumps) {
@@ -37,4 +48,9 @@ function bestJump(jumps) {
     });
     return best;
 }
-console.log(" the best jump is :",bestJump(jumps));
+console.log(" the best jump is :", bestJump(jumps));
+
+
+console.log(avgJumper(jumps2));
+console.log("best avg jumper is :", bestAvgJump(avgJumper(jumps2)));
+console.log(" the best jump is :", bestJump(jumps2));
