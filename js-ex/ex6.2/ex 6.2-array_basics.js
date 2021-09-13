71ddbe7316ef194bf41af5d6e1e58710b259e591
asadhmeed <asadhmeed1@gmail.com>
2021-09-13 15:35:08 +0300

    add ex6.2

---
+++ js-ex/ex6.2/ex 6.2-array_basics.js
@@ -0,0 +1,27 @@
+const people = ["Greg", "Mary", "Devon", "James"];
+
+people.forEach(name => {
+    console.log('the name is : ',name);
+});
+
+people.shift();
+people.pop();
+people.unshift("Matt");
+people.push("Asad");
+
+
+
+for (let i = 0; i < people.length; i++) {
+    console.log("in for loop : ", people[i]);
+    if (people[i]==="Mary") {
+        break;
+    }
+}
+
+
+
+let newPeople = people.slice(2);
+console.log(newPeople);
+
+console.log("Mary located in index",people.indexOf('Mary'));
+console.log("foo located in index",people.indexOf('foo'));
