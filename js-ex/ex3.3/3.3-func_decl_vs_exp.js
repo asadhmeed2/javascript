44503117298902f87fe31e8768e463c3ec8ca9ef
asadhmeed <asadhmeed1@gmail.com>
2021-09-13 09:28:35 +0300

    ex3.3 func vs exp

---
+++ js-ex/ex3.3/3.3-func_decl_vs_exp.js
@@ -0,0 +1,21 @@
+
+const worldPopulation = 7900;
+// population must etered in milions 
+function percentageOfWorld1(population){
+
+    return (population/worldPopulation)*100;
+
+}
+console.log("usas percentage of the world populattion is : ",percentageOfWorld1(328.2));
+console.log("Isreali percentage of the world populattion is : ",percentageOfWorld1(9.053));
+console.log("egyption percentage of the world populattion is : ",percentageOfWorld1(100.4));
+
+
+const percentageOfWorld2= function (population){
+
+    return (population/worldPopulation)*100;
+    
+}
+console.log("usas percentage of the world populattion is : ",percentageOfWorld2(328.2).toFixed(3));
+console.log("Isreali percentage of the world populattion is : ",percentageOfWorld2(9.053).toFixed(3));
+console.log("egyption percentage of the world populattion is : ",percentageOfWorld2(100.4).toFixed(3));
