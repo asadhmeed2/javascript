5a89750ded7a8766dda3abe8288facdb8e12f3a6
asadhmeed <asadhmeed1@gmail.com>
2021-09-14 14:07:05 +0300

    add ex7.5

--- js-ex/ex7.5/ex7.5-count_the_letters.js
+++ js-ex/ex7.5/ex7.5-count_the_letters.js
@@ -7,15 +7,33 @@ const array = ["Hello", "Good Day", "Your Welcome", "hotdog",
     const counteTheLetter=(array)=>{
         const letters={}
         let string=array.join('');
-        string.split('').forEach(letter => {
-            if(letters[letter]){
-                letters[letter]+=1;
+        string.split('').forEach(letter => {//iterate on all the array and count the letters
+            if(letters[letter.toLowarCase()]){//if the letter is a proporty in the object 
+                letters[letter.toLowarCase()]+=1; // add 1 to the value of the proporty
             }else{
-                letters[letter] =1;
+                letters[letter.toLowarCase()] = 1; // else add new proporty to the object and initialize it with value 1
             }
             
         });
         return letters;
     }
+let newLetters = counteTheLetter(['asad', "hemeed"])
+console.log(newLetters);
 
-console.log(counteTheLetter(['asad',"hmeed"]));
+
+
+
+
+let maxLetter ='z';
+let maxRepeted=0;
+for (const key in newLetters) {
+    
+    if (newLetters.hasOwnProperty( key)) {
+        if (newLetters[''+key] >maxRepeted) {
+            maxLetter=''+key;
+            maxRepeted=newLetters[''+key];
+        }
+        
+    }
+}
+console.log(maxLetter);
