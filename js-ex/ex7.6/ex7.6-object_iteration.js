36bfa92a3ef9ac5f0855df2da73b5c727dd2c947
asadhmeed <asadhmeed1@gmail.com>
2021-09-14 14:18:33 +0300

    add ex7.6

---
+++ js-ex/ex7.6/ex7.6-object_iteration.js
@@ -0,0 +1,18 @@
+let swap=(object)=>{
+    let newObject={}
+for (const key in object) {
+    if (object.hasOwnProperty( key)) {
+        newObject[object[""+key]] = ''+key;
+        
+    }
+    
+}
+return newObject;
+}
+
+const object ={
+    name:'asad',
+    age:30
+}
+console.log(object);
+console.log(swap(object));
