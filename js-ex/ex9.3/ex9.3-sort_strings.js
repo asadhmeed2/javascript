3a626159e32347f1773df66dcf0bd3293da23455
asadhmeed <asadhmeed1@gmail.com>
2021-09-19 11:12:11 +0300

    update ex9.3

--- js-ex/ex9.3/ex9.3-sort_strings.js
+++ js-ex/ex9.3/ex9.3-sort_strings.js
@@ -1,5 +1,5 @@
 const foods = ["falafel", "sabich", "hummus", "pizza with extrapineapple"];
-const foodsCopy =[...foods];
+const foodsCopy = [...foods];
 //ascending
 foods.sort()
 
@@ -9,6 +9,34 @@ foodsCopy.sort();
 foodsCopy.reverse();
 
 
-console.log(foods);
-console.log(foodsCopy);
+console.log("foods ascending order",foods);
+console.log("foods descending order",foodsCopy);
 
+
+
+const foodsWithUpperCase = [
+    "falafel",
+    "Sabich",
+    "hummus",
+    "pizza with extra pineapple",
+];
+foodsWithUpperCaseCoby = [...foodsWithUpperCase];
+foodsWithUpperCase.sort();
+foodsWithUpperCase.reverse();
+
+foodsWithUpperCaseCoby.sort();
+
+
+
+console.log("foods with upper case ascending order", foodsWithUpperCase);
+console.log("foods with upper case descending order", foodsWithUpperCaseCoby);
+
+
+
+// longest word
+const words = ["apple", "supercalifragilisticexpialidocious",
+    "hi", "zoo"];
+
+
+    words.sort((a,b)=> a.length - b.length);
+console.log("sort according to the word length ascendingly",words);
