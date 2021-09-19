b9e118ba165b429a645f3775d4f96087e4ef470e
asadhmeed <asadhmeed1@gmail.com>
2021-09-19 10:56:19 +0300

    add ex9.3

---
+++ js-ex/ex9.3/ex9.3-sort_strings.js
@@ -0,0 +1,14 @@
+const foods = ["falafel", "sabich", "hummus", "pizza with extrapineapple"];
+const foodsCopy =[...foods];
+//ascending
+foods.sort()
+
+
+// descending
+foodsCopy.sort();
+foodsCopy.reverse();
+
+
+console.log(foods);
+console.log(foodsCopy);
+
