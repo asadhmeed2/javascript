1733d9d1ffe701d7dea80fa7c442fd1396de0f70
asadhmeed <asadhmeed1@gmail.com>
2021-09-20 15:13:46 +0300

    update ex14.3

--- js-ex/ex14.3/ex14.3-sum_of_even.js
+++ js-ex/ex14.3/ex14.3-sum_of_even.js
@@ -1,5 +1,5 @@
 function getSumOfEven(arr) {
-    return arr[2] + arr[4] + arr[6] + arr[8] 
+    return arr[1] + arr[3] + arr[5] + arr[7] + arr[9];
 }
 getSumOfEven([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
 console.log(getSumOfEven([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]));
