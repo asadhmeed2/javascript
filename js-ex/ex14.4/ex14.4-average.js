9c24800a086dd24593e6a35a513e42181203fed6
asadhmeed <asadhmeed1@gmail.com>
2021-09-20 15:30:02 +0300

    update ex14.4

--- js-ex/ex14.4/ex14.4-average.js
+++ js-ex/ex14.4/ex14.4-average.js
@@ -4,7 +4,8 @@ function calcAverage(arr) {
     for (var i = 0; i < arr.length; i++) {
         sum += arr[i];
     }
-    return sum;
+    // return sum;
+    return sum/i;
 }
 calcAverage([85, 90, 92]);
 console.log(calcAverage([85, 90, 92]));
@@ -13,9 +14,10 @@ console.log(calcAverage([85, 90, 92]));
 
 // 1. First find the line that contains the problem.Write it
 // down.
-// 2
+// 2 ,7
 // 2. Which debug method did you use to find the bug ?
 // vs code debuger
 // 3. Explain the bug in your own words.
 //  the sum  variable was not initialize with number value and canot add to it numbers  and thats what caused the NaN output
+// and in the return statment need to divid by arr.length to get the average
 // 4. Fix the code and submit it all.
