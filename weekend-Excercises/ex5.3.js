025d67caf5b794a3b9a1a1e1ee6781e4365cd8e6
asadhmeed <asadhmeed1@gmail.com>
2021-09-24 23:31:20 +0300

    weekend-Excercises ex5.3

---
+++ weekend-Excercises/ex5.3.js
@@ -0,0 +1,15 @@
+const toCamelCase =(string)=>{
+    let array = string.split(/-|_/g);
+    let first =true;
+    for (let i=0;i<array.length;i++) {
+        if (i!==0) {
+            array[i] = array[i].replace(array[i][0], array[i][0].toUpperCase())
+        }
+        
+    }
+    return array.join('');
+}
+
+console.log(toCamelCase("the-stealth-warrior"));
+console.log(toCamelCase("the_stealth-warrior"));
+console.log(toCamelCase("the_stealth_warrior"));
