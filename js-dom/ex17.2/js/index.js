718ce8357dd54cdda3856e50de6aea50f96910b9
newasad <asadhmeed2@gmail.com>
2021-09-26 14:14:36 +0300

    add 17.2

---
+++ js-dom/ex17.2/js/index.js
@@ -0,0 +1,30 @@
+const textArea1 = document.querySelector('#textArea1');
+const submit = document.querySelector('#submit-btn');
+const textWorning = document.querySelector('#worning');
+
+
+const textAreaCheck =()=>{
+    console.log(textArea1.value);
+    if (textArea1.value.length <100) {
+        
+        textWorning.innerHTML ='worning enter at least 100 characters '
+        textWorning.style.color ='red';
+        
+    }else {
+        
+        textWorning.innerHTML = ""
+    }
+}
+
+
+submit.addEventListener('click',()=>{
+    textAreaCheck();
+})
+textArea1.addEventListener('keyup', (event)=>textAreaCheck());
+// textArea1.addEventListener('focus',()=>textAreaCheck());
+
+
+
+
+
+
