650f1ac7edde5b75c573e98e3b4d3e767ad3e99a
newasad <asadhmeed2@gmail.com>
2021-09-27 15:32:53 +0300

    update ex19.2

---
+++ js-dom/ex19.2/js/index.js
@@ -0,0 +1,55 @@
+const form = document.querySelector('[name="verify"]');
+const inputs = document.querySelectorAll('.input')
+console.log(form);
+const checkInputLength = (length) => length > 1 && length <= 6;
+
+const checkIfItsNumber = (char) => Number(char) ? true : false;
+
+const checkInputAndMoveToNaxtSibling = (e) => {
+    let input = e.target;
+    console.log("in input event");
+
+
+
+    if (!input.nextElementSibling && input.value) {
+        e.target.value = e.target.value[0] >= 0 ? e.target.value[0] : '';
+
+    } else
+        if (input.nextElementSibling && input.value) {
+            if (input.value >= 0 && input.value <= 9) {
+                e = input.nextElementSibling.focus();
+            } else {
+                console.log('19 input :', input.value);
+                e.target.value = Number(e.target.value) ? e.target.value[0] : '';
+            }
+        } else if (input.previousElementSibling && !input.value) {
+            e = input.previousElementSibling.focus();
+        }
+
+}
+
+
+const handlePaste = (e) => {
+
+    console.log("in paste event");
+    const paste = e.clipboardData.getData('text');
+    console.log(paste);
+
+    inputs.forEach((input, i) => input.value = paste[i] >= 0 && paste[i] <= 9?paste[i]:"");
+
+
+}
+
+
+
+
+form.addEventListener('input', e => {
+    checkInputAndMoveToNaxtSibling(e);
+});
+inputs[0].addEventListener('paste', e => {
+    
+        handlePaste(e)
+    
+}
+)
+
