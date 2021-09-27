9d20bd0713037d731cc494ed40c16aad2913806a
newasad <asadhmeed2@gmail.com>
2021-09-27 17:02:26 +0300

    update ex21.1

---
+++ js-dom/ex21.1/js/index.js
@@ -0,0 +1,12 @@
+const btn =document.querySelector('#check-btn')
+const age =document.querySelector('#age')
+const message =document.querySelector('.message')
+
+
+const chekAge =(age)=>age<18;
+
+const onclick =(e)=>{
+    message.innerHTML = chekAge(age.value) ? `<span class="red"> you’re too young</span>` :`<span class="green"> you can drink appear<span class="red"> but its not halthy</span></span>`;
+}
+
+btn.addEventListener('click',onclick)
