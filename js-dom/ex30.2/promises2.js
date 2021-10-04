54f6fea1776d603a098190a746c2bd6318118493
newasad <asadhmeed2@gmail.com>
2021-10-04 13:50:16 +0300

    add ex30.2

--- js-dom/ex30.2/promises2.js
+++ js-dom/ex30.2/promises2.js
@@ -1,51 +1,43 @@
+
 function makeAllCaps(arrayOfStrings) {
     return new Promise((resolve, reject) => {
-        let notString = arrayOfStrings.find((element) => typeof element !== 'string')
-        if (!notString) {
-
-            resolve(arrayOfStrings.map(word => {
-                return word.toUpperCase();
-            }))
-        } else {
-            reject('error array is not array of strings')
-        }
-    });
+        resolve(arrayOfStrings.map(word => {
+            if (typeof word !== "string") {
+                reject('error array is not array of strings');
+            }
+            return word.toUpperCase();
+        }))
+    })
 }
 function sortWords(arrayOfStrings) {
     return new Promise((resolve, reject) => {
-        let notString = arrayOfStrings.find((element) => typeof element !== 'string')
-        if (!notString) {
-            let array = [...arrayOfStrings];
-            resolve(array.sort());
-        } else {
-            reject('error array is not array of strings')
-        }
+        resolve([...arrayOfStrings].map((word) => {
+            return typeof word === 'string' ? word : reject("error array is not array of strings")
+        }).sort((a, b) => a.localeCompare(b)));
     });
 }
-
-
-
-
 makeAllCaps(["asad", 'hmeed', 'wajde']).then((data) => {
-    console.log(data);
+    sortWords(data).then((data) => {
+        console.log(data);
+    }).catch(err => {
+        console.log(err);
+    })
 }).catch(err => {
     console.log(err);
 })
 makeAllCaps(["asad", 'hmeed', 2, 'wajde']).then((data) => {
-    console.log(data);
+    sortWords(data).then((data) => {
+        console.log(data);
+    }).catch(err => {
+        console.log(err);
+    })
 }).catch(err => {
     console.log(err);
 })
 
-sortWords(["asad", 'hmeed', 'amjd', 'wajde']).then((data) => {
-    console.log(data);
-}).catch(err => {
-    console.log(err);
-})
-sortWords(["asad", 'hmeed', 2, 'wajde']).then((data) => {
-    console.log(data);
-}).catch(err => {
-    console.log(err);
-})
+
+
+
+
 
 
